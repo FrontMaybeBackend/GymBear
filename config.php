7a4ea8ace211aba954ea7ea1f3400aa85e10226b
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-01-17 12:44:17 +0100

    change from mysql to PDO

--- config.php
+++ config.php
@@ -4,11 +4,14 @@ $host = "localhost";
 $db_user = "root";
 $db_password = "";
 $db_name = "gymweb";
-$linkDB= mysqli_connect($host, $db_user, $db_password, $db_name);
+$linkDB = mysqli_connect($host, $db_user, $db_password, $db_name);
 
-if(mysqli_connect_error()){
-    die("There was an error while connecting to database");
 
-}else {
-  //  echo  " succesfully connected to database";
+
+try {
+  $linkDB = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+  $linkDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "success";
+} catch (PDOException $e) {
+  echo "failed:" . $e->getMessage();
 }
