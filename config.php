796eabac0c37a4e7403c8c940b1de3b067fc95dd
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-01-17 15:25:20 +0100

    remove redundant code

--- config.php
+++ config.php
@@ -4,13 +4,13 @@ $host = "localhost";
 $db_user = "root";
 $db_password = "";
 $db_name = "gymweb";
-$linkDB = mysqli_connect($host, $db_user, $db_password, $db_name);
+
 
 
 
 try {
-  $linkDB = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
-  $linkDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "success";
 } catch (PDOException $e) {
   echo "failed:" . $e->getMessage();
